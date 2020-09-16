936fb301506c71617c3ab9d8a2de67e5ac6cda31
Abbas Siddiqui <sidiki97@gmail.com>
2020-09-16 11:36:44 -0400

    Add Basics directory

---
+++ theBasics/basics.js
@@ -0,0 +1,13 @@
+// 1.
+let firstName = "Abbas";
+let lastName = "Siddiqui";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+// 6.
+console.log(Number('5')+10);
+
+// 7. 
+console.log('The value of 5 + 10 is ' + String(Number('5') + 10));
