391ead276cb36cedfcaa92294f647e20ca58d7e1
Abbas Siddiqui <sidiki97@gmail.com>
2020-09-16 14:56:37 -0400

    Finished Basics

--- theBasics/basics.js
+++ theBasics/basics.js
@@ -11,3 +11,14 @@ console.log(Number('5')+10);
 
 // 7. 
 console.log('The value of 5 + 10 is ' + String(Number('5') + 10));
+
+// 8.
+let foo = ['a', 'b', 'c'];
+foo.length;  // returns 3
+foo[3];      // will this result in an error? - NO 
+// RETURNS undefined
+
+// 13.
+console.log('12' < '9');
+// returns true since character comparison is occurring
+// first comparison between '1' and '9' characters
