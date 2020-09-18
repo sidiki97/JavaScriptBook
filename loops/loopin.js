db7b11e600c6531b01b34abefb60a2ccd36d9230
Abbas Siddiqui <sidiki97@gmail.com>
2020-09-18 11:24:00 -0400

    Do-while and Recursion

--- loops/loopin.js
+++ loops/loopin.js
@@ -5,17 +5,44 @@ function times(number1, number2) {
   return result;
 }
 
-function factorial(number) {
-  let total = 1;
-  for (var i = 1; i <= number; i++) {
-    total = total * i;
-  }
 
-  return total;
-}
 
+/*
 console.log(factorial(8));
 
 for (var i = 0; i < 5;) {
   
 }
+
+function randomNumberBetween(min, max) {
+  return result = Math.floor(Math.random() * (max - min + 1) + min);
+  
+}
+
+let tries = 0;
+let result;
+
+
+do {
+  result = randomNumberBetween(1, 6);
+  tries += 1;
+} while (result <= 2);
+
+console.log('It took ' + String(tries) + ' tries to get a number greater than 2');
+
+let answer;
+let q = require('readline-sync');
+do {
+  answer = q.question("Do you want to do this again? ");
+} while (answer == 'y');
+
+*/
+
+function factorial(number) {
+  if (number == 1) {
+    return 1;
+  }
+  return number * factorial(number - 1);
+}
+
+console.log(factorial(5));
